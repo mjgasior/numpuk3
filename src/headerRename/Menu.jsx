0e047037f8d6630b1717c1831747edad2669b754
Michal J. Gasior <michaljgasior@gmail.com>
2020-05-01 22:03:01 +0200

    Rename header dir

--- src/headerRename/Menu.jsx
+++ src/headerRename/Menu.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 import CloudUploadIcon from "@material-ui/icons/CloudUpload";
+import ListIcon from "@material-ui/icons/List";
 import { ButtonLink } from "./ButtonLink";
 
 export const Menu = () => {
   return (
     <>
       <ButtonLink to="/">
-        <AddShoppingCartIcon />
+        <ListIcon />
       </ButtonLink>
       <ButtonLink to="/books">
         <CloudUploadIcon />
