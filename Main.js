c22062cc6042cdae482cc7be291264c88932ea5f
Michal J. Gasior <michaljgasior@gmail.com>
2020-05-01 13:40:15 +0200

    Add DevTools

--- Main.js
+++ Main.js
@@ -1,11 +1,14 @@
 const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
-  // Create the browser window.
+  // create the browser window.
   win = new BrowserWindow({ width: 800, height: 600 });
 
-  // and load the index.html of the app.
+  // and load the index.html of the app
   win.loadFile("index.html");
+
+  // open the DevTools
+  win.webContents.openDevTools();
 }
 
 app.on("ready", createWindow);
