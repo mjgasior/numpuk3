1a7e6711c37460ea7ce8865535be582dfeb7b943
Michal J. Gasior <michaljgasior@gmail.com>
2020-05-02 00:44:36 +0200

    Button to invoke modal

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { Header } from "./header/Header";
 import { theme } from "./+utils/theme";
 import { UploadPage } from "./upload/UploadPage";
 import { ExaminationsPage } from "./examinations/ExaminationsPage";
-import { dialog } from "./+utils/electron";
 
 const Container = styled.div`
   display: flex;
@@ -13,18 +12,23 @@ const Container = styled.div`
   margin: 10px;
 `;
 
-export const App = () => {
-  console.log("dialog");
-  console.log(dialog);
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
+`;
 
+export const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Container>
         <Header />
-        <Switch>
-          <Route path="/upload" component={UploadPage} />
-          <Route path="/" component={ExaminationsPage} />
-        </Switch>
+        <Content>
+          <Switch>
+            <Route path="/upload" component={UploadPage} />
+            <Route path="/" component={ExaminationsPage} />
+          </Switch>
+        </Content>
       </Container>
     </ThemeProvider>
   );
--- src/upload/UploadPage.jsx
+++ src/upload/UploadPage.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { showFileDialog } from "./showFileDialog";
+import Button from "@material-ui/core/Button";
 
 export function UploadPage() {
   return (
     <div>
-      <h2>Upload page</h2>
-      <p>
-        This is the environment build: <i>{process.env.NODE_ENV}</i>
-      </p>
-      <button onClick={showFileDialog}>Show dialog</button>
+      <Button variant="contained" color="primary" onClick={showFileDialog}>
+        Wybierz lokalizację
+      </Button>
     </div>
   );
 }
