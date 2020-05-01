34bd35b6b67fa39a60d613c0f0b3f6d7f7c94a3a
Michal J. Gasior <michaljgasior@gmail.com>
2020-05-01 21:08:28 +0200

    Router small setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import styled, { ThemeProvider } from "styled-components";
 import { Header } from "./+components/Header";
 import { theme } from "./+utils/theme";
+import { Home } from "./+components/Home";
+import { Books } from "./+components/Books";
+import Navigation from "./+components/Navigation";
 
 const Container = styled.div`
   display: flex;
@@ -13,6 +17,12 @@ export const App = () => {
     <ThemeProvider theme={theme}>
       <Container>
         <Header />
+        <Navigation />
+
+        <Switch>
+          <Route path="/books" component={Books} />
+          <Route path="/" component={Home} />
+        </Switch>
       </Container>
     </ThemeProvider>
   );
