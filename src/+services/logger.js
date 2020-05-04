0787fe2564cec19ef199bf841121541fe4cef5f9
Michal J. Gasior <michaljgasior@gmail.com>
2020-05-04 18:10:47 +0200

    Change route of logs

--- src/+services/logger.js
+++ src/+services/logger.js
@@ -1,7 +1,27 @@
 import { log } from "../+apis/dependenciesApi";
 
+const currentLogKeys = [];
+
+const warn = (message) => {
+  currentLogKeys.push(message);
+  log.warn(message);
+};
+
+const error = (message) => {
+  currentLogKeys.push(message);
+  log.error(message);
+};
+
+const info = (message) => {
+  currentLogKeys.push(message);
+  log.info(message);
+};
+
+const getLogs = () => currentLogKeys;
+
 export const logger = {
-  warn: log.warn,
-  error: log.error,
-  info: log.info,
+  warn,
+  error,
+  info,
+  getLogs,
 };
