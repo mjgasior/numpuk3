462bc30c07d726971d616e59e260fe5a3839a40c
Michal J. Gasior <michaljgasior@gmail.com>
2020-05-07 20:39:13 +0200

    Single view flattened

--- src/reader/singleExamination/SingleExamination.jsx
+++ src/reader/singleExamination/SingleExamination.jsx
@@ -4,19 +4,27 @@ import { SpecificDataTable } from "./+components/SpecificDataTable";
 import Grid from "@material-ui/core/Grid";
 
 export const SingleExamination = ({ examination }) => {
-  const { results, ...rest } = examination;
+  const {
+    results,
+    examinationType,
+    ph,
+    consistency,
+    bacteriaCount,
+    extendedResults,
+    ...metadata
+  } = examination;
 
   return (
     <Grid container spacing={3}>
       <Grid item xs={6}>
-        <ObjectTable data={rest} />
+        <ObjectTable data={metadata} />
       </Grid>
       <Grid item xs={6}>
         <Grid container spacing={3} direction="column">
           <Grid item xs={12}>
             <SpecificDataTable
-              data={rest}
-              extendedResults={rest.extendedResults}
+              data={{ examinationType, ph, consistency, bacteriaCount }}
+              extendedResults={extendedResults}
             />
           </Grid>
           <Grid item xs={12}>
