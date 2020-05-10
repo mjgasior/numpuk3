d3d0fce867ae4cc5558adb79223f88048528ee6a
Michal J. Gasior <michaljgasior@gmail.com>
2020-05-10 22:03:28 +0200

    Fix

--- src/examinationGrid/+hooks/usePagination.jsx
+++ src/examinationGrid/+hooks/usePagination.jsx
@@ -1,14 +1,16 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 export const usePagination = () => {
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(25);
-  const [pagination] = useState({
-    page,
-    setPage,
-    rowsPerPage,
-    setRowsPerPage,
-  });
 
-  return pagination;
+  return useMemo(
+    () => ({
+      page,
+      setPage,
+      rowsPerPage,
+      setRowsPerPage,
+    }),
+    [page, rowsPerPage]
+  );
 };
