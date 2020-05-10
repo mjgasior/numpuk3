528d383fb1afbadd1f336a3a3b461900101de8ea
Michal J. Gasior <michaljgasior@gmail.com>
2020-05-10 13:00:31 +0200

    Fixed grid

--- src/examinationGrid/Table.jsx
+++ src/examinationGrid/Table.jsx
@@ -1,14 +1,41 @@
 import React from "react";
-import { AutoSizer, Grid } from "react-virtualized";
+import {
+  AutoSizer,
+  CellMeasurer,
+  CellMeasurerCache,
+  Grid,
+} from "react-virtualized";
 import { useTranslation } from "react-i18next";
+
+const cache = new CellMeasurerCache({
+  defaultWidth: 100,
+  minWidth: 100,
+  fixedHeight: true,
+});
 
 export const Table = React.memo(({ examinations }) => {
   const { t } = useTranslation("n3_metadata");
-  const cellRenderer = ({ columnIndex, key, rowIndex, style }) => {
+  const cellRenderer = ({ columnIndex, key, parent, rowIndex, style }) => {
     return (
-      <div key={key} style={style}>
-        {t(examinations[rowIndex][columnIndex])}
-      </div>
+      <CellMeasurer
+        cache={cache}
+        columnIndex={columnIndex}
+        key={key}
+        parent={parent}
+        rowIndex={rowIndex}
+      >
+        <div
+          key={key}
+          style={{
+            ...style,
+            height: 35,
+            whiteSpace: "nowrap",
+            padding: "5px",
+          }}
+        >
+          {t(examinations[rowIndex][columnIndex])}
+        </div>
+      </CellMeasurer>
     );
   };
 
@@ -18,7 +45,7 @@ export const Table = React.memo(({ examinations }) => {
         <Grid
           cellRenderer={cellRenderer}
           columnCount={examinations[0].length}
-          columnWidth={200}
+          columnWidth={cache.columnWidth}
           height={height}
           rowCount={examinations.length}
           rowHeight={30}
