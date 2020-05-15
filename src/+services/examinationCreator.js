5633f3a9e49a3bf1a9d79f4269cac23dea98db52
Michal J. Gasior <michaljgasior@gmail.com>
2020-05-16 00:32:33 +0200

    Sample data

--- src/+services/examinationCreator.js
+++ src/+services/examinationCreator.js
@@ -31,18 +31,28 @@ export const saveExaminations = async (
 ) => {
   console.log(directory);
   const workbook = createWorkbook();
-  const sheet = workbook.addWorksheet("Wyniki");
+  const worksheet = workbook.addWorksheet("Wyniki");
 
   const data = await getExaminations({}, metadataFilters, {}, testFilters, {
     page: 0,
     rowsPerPage: 100,
   });
 
+  worksheet.columns = [
+    { header: "Id", key: "id", width: 10 },
+    { header: "Name", key: "name", width: 32 },
+    { header: "D.O.B.", key: "DOB", width: 10, outlineLevel: 1 },
+  ];
+
+  worksheet.addRow({ id: 1, name: "John Doe", dob: new Date(1970, 1, 1) });
+  worksheet.addRow({ id: 2, name: "Jane Doe", dob: new Date(1965, 1, 7) });
+
+  /*
   data.examinations.forEach((element) => {
     const { results, ...rest } = element;
     sheet.addRow(rest);
   });
-
+*/
   console.log(data);
 
   await workbook.xlsx.writeFile(directory + "/wyniki.xlsx");
